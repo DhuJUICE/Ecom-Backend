c2ce7247119a5edbcc502db48cb5dd67143cddee
DhuJUICE <junierherandien84@gmail.com>
2025-07-12 12:13:48 +0200

    Update models.py

--- ecom_project/product_management/models.py
+++ ecom_project/product_management/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class PRODUCT(models.Model):
 
-	#product details for database
-	prodName = models.CharField(max_length = 100)
-	prodPrice = models.DecimalField(max_digits=10, decimal_places=2)
-	prodDesc = models.CharField(max_length = 100)
-	prodAvailQuant = models.IntegerField()
-	prodOnMenu =  models.BooleanField(default=False)
-	prodImagePath = models.CharField(max_length=255, blank=True, null=True)
-	approved = models.BooleanField(default=False)
+    #product details for database
+    prodName = models.CharField(max_length = 100)
+    prodPrice = models.DecimalField(max_digits=10, decimal_places=2)
+    prodDesc = models.CharField(max_length = 100)
+    prodAvailQuant = models.IntegerField()
+    prodOnMenu =  models.BooleanField(default=False)
+    prodImagePath = models.CharField(max_length=255, blank=True, null=True)
+    approved = models.BooleanField(default=False)
