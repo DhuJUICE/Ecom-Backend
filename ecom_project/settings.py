8d0a8f1794dab698206820fca13a7a20883caa98
DhuJUICE <102233050+DhuJUICE@users.noreply.github.com>
2025-10-03 23:26:53 +0200

    Update settings.py

--- ecom_project/settings.py
+++ ecom_project/settings.py
@@ -69,8 +69,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",  # React app origin
     "http://localhost:3000",
-    'https://yummytummies.onrender.com', #deployed fronted
-    'https://yummytummies-frontend2.onrender.com', #second backup deployed frontend
+    'https://heranbites.onrender.com', #deployed fronted
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -81,12 +80,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-			'browse_management/html_templates',
-			'api_management/html_templates',
-			'checkout_management/html_templates',
-			'cart_management/html_templates',
-			'product_management/html_templates',
-			'user_management/templates/user_management',
+			'api_management/html_templates
 		],
         'APP_DIRS': True,
         'OPTIONS': {
